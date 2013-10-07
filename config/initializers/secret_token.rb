c66a691d84aa69bb4256986b5ab99fb269bd8a56
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-10-06 22:13:38 -0400

    set up rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vsme::Application.config.secret_key_base = 'bf12f5fd46f74dc573e0874cd94dc74f167cb7000aedc5e4a25e7c21ec7be93f48307b47e22afb74dc3a10684f8f7d802056aba92ed6abafd8bddd51d647131e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vsme::Application.config.session_store :cookie_store, key: '_vsme_session'
