138f6b94bfcaebd9b1a7542b3a8a3fd1f0420f94
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-10-12 15:53:12 -0400

    fixed win loss logic and handled tie

--- app/services/determines_winner.rb
+++ app/services/determines_winner.rb
@@ -1,28 +1,26 @@
 class DeterminesWinner
 
-  attr_reader :game_one_id,
-              :game_two_id,
-              :game_one,
+  attr_reader :game_one,
               :game_two,
               :last_result
 
-  def self.with
-    DeterminesWinner.new
+  def self.with last_result
+    DeterminesWinner.new last_result
   end
 
-  def initialize
-    puts "called determines winner"
-    last_result = Result.last
-    game_one_id = last_result.game_one_id
-    game_two_id = last_result.game_two_id
-    game_one = Game.find(game_one_id)
-    game_two = Game.find(game_two_id)
+  def initialize last_result
+    last_result = last_result
+    game_one = Game.find(last_result.game_one_id)
+    game_two = Game.find(last_result.game_two_id)
 
-    if game_one.score >> game_two.score
+    if game_one.score < game_two.score
+      last_result.winning_user_id = game_two.user_id
+      last_result.save
+    elsif game_one.score > game_two.score
       last_result.winning_user_id = game_one.user_id
       last_result.save
     else
-      last_result.winning_user_id = game_two.user_id
+      last_result.winning_user_id = -1
       last_result.save
     end
   end
