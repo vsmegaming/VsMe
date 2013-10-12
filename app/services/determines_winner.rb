f97d409a2c72ebb94877fcdc0f4f5dda2ddb0ba7
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-10-12 16:29:11 -0400

    transfer credits from loser to winner

--- app/services/determines_winner.rb
+++ app/services/determines_winner.rb
@@ -12,13 +12,23 @@ class DeterminesWinner
     last_result = last_result
     game_one = Game.find(last_result.game_one_id)
     game_two = Game.find(last_result.game_two_id)
+    user_one = User.find(game_one.user_id)
+    user_two = User.find(game_two.user_id)
 
     if game_one.score < game_two.score
       last_result.winning_user_id = game_two.user_id
       last_result.save
+      user_one.credits -= 5
+      user_one.save
+      user_two.credits += 5
+      user_two.save
     elsif game_one.score > game_two.score
       last_result.winning_user_id = game_one.user_id
       last_result.save
+      user_one.credits += 5
+      user_one.save
+      user_two.credits -= 5
+      user_two.save
     else
       last_result.winning_user_id = -1
       last_result.save
