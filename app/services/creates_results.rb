3c6eea52d7a66737e39d4010326cacdc326c3cfd
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-10-12 15:53:01 -0400

    user can't play against self

--- app/services/creates_results.rb
+++ app/services/creates_results.rb
@@ -10,20 +10,40 @@ class CreatesResults
 
   def initialize game
     @game = game
-    last_result = Result.last
+    @results = Result.all
+    last_result = nil
 
-    if last_result && last_result.game_two_id == nil
-      last_result.game_two_id = @game.id
-      last_result.save
-      @game.result_id = last_result.id
-      @game.save
-      DeterminesWinner.with()
+    if @results.any?
+      @results.each do |result|
+        if result.game_two_id == nil
+          last_result = result
+          last_result_game = Game.find(last_result.game_one_id)
+          if last_result_game.user_id != @game.user_id
+            last_result.game_two_id = @game.id
+            last_result.save
+            @game.result_id = last_result.id
+            @game.save
+            DeterminesWinner.with(last_result)
+            break
+          else
+            new_result
+            break
+          end
+        end
+      end
+      if last_result == nil
+        new_result
+      end
     else
-      new_result = Result.create(game_one_id: @game.id)
-      @game.result_id = new_result.id
-      @game.save
+      new_result
     end
 
+  end
+
+  def new_result
+    new_result = Result.create(game_one_id: @game.id)
+    @game.result_id = new_result.id
+    @game.save
   end
 
 end
