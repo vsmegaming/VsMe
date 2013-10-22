f927ff16c5f38c3c87e87a022e757c33705fea0b
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-10-21 22:01:50 -0400

    simple canvas implementation

---
+++ app/assets/javascripts/game.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+  console.log( "ready!" );
+
+  function draw(){
+    var canvas = document.getElementById('tutorial');
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect (10, 10, 55, 50);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect (30, 30, 55, 50);
+  }
+  
+  draw();
+
+});
