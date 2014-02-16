7f0e7938b2a64febaece4bc955ec4656022c0eb7
Don Pawlowski <dpawlowski@designinginteractive.com>
2014-02-16 12:41:00 -0500

    use user name instead of email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216162009) do
+ActiveRecord::Schema.define(version: 20140216170407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20140216162009) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "wins",                   default: 0
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
