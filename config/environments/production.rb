5fd1a35e34771dcf9343efa4ea211ee3e679318c
Kelvin Wong <kwong691@gmail.com>
2014-03-01 01:53:13 +0000

    Try email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,21 @@ Vsme::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { :host => 'http://vsmegaming.com' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: SERVER,
+    port: PORT,
+    domain: 'vsmegaming.com',
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: EMAIL,
+    password: PW
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
